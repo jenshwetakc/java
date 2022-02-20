18d92a263a0bd7d8b911640a154db2c99a3ddb15
shweta kc <devshwetakc@hotmail.com>
2022-02-20 11:04:13 +0545

    busted max value -overflow

--- ByteShortIntLong/src/Main.java
+++ ByteShortIntLong/src/Main.java
@@ -10,6 +10,8 @@ public class Main {
 
         System.out.println("Busted minimum value = " + (myMinIntValue-1));
 
+        System.out.println("Busted maximum value = " + (myMaxIntValue +1));
+
     }
 
 }
