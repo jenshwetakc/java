2c082ebaba7f881c636e1b6b14285674f84a6b8a
shweta kc <devshwetakc@hotmail.com>
2022-02-20 10:55:22 +0545

    minimum value

---
+++ ByteShortIntLong/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public  static  void main(String[] args){
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        System.out.println("Integer minimum value = " + myMinIntValue);
+
+    }
+
+}
