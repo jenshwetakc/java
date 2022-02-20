29bb42be574496e43fd03ca3c859efe55654e5d0
shweta kc <devshwetakc@hotmail.com>
2022-02-20 12:53:05 +0545

    Add min max float value

---
+++ FloatDouble/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code
+        float myMinFloatValue = Float.MIN_VALUE;
+        System.out.println("min float value  = " + myMinFloatValue);
+
+        float myMaxFloatValue = Float.MIN_VALUE;
+        System.out.println("min float value  = " + myMaxFloatValue);
+    }
+}
